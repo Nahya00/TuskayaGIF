f4924a21a3c520b50708e1b42751945b29458c1e
Nahya00 <jujulecarambar@gmail.com>
2025-06-11 01:44:57 +0200

    Update anti_link_bot.py

--- anti_link_bot.py
+++ anti_link_bot.py
@@ -8,7 +8,7 @@ intents.message_content = True
 
 bot = discord.Client(intents=intents)
 
-url_regex = re.compile(r'https?://[^\s]+')
+url_regex = re.compile(r'(https?://[^\s]+)')
 whitelist_domains = ["tenor.com", "media.tenor.com", "giphy.com", "media.giphy.com"]
 
 @bot.event
@@ -23,26 +23,40 @@ async def on_message(message):
     urls = url_regex.findall(message.content)
     for url in urls:
         domain = url.split('/')[2]
-        embed = discord.Embed()
-        embed.set_author(
-            name=str(message.author),
-            icon_url=message.author.avatar.url if message.author.avatar else message.author.default_avatar.url
-        )
 
         if any(domain.endswith(allowed) for allowed in whitelist_domains):
-            # GIF autorisé => envoyer embed bleu
-            embed.description = "**GIF envoyé.**"
-            embed.color = discord.Color.dark_blue()
-            await message.channel.send(embed=embed, delete_after=5)
-        else:
-            # Lien interdit => supprimer + embed rouge
             try:
                 await message.delete()
-                embed.description = "**Les liens ne sont pas autorisés ici.**"
-                embed.color = discord.Color.red()
+
+                embed = discord.Embed(
+                    description="**GIF détecté ✅**",
+                    color=discord.Color.dark_blue()
+                )
+                embed.set_author(
+                    name=str(message.author),
+                    icon_url=message.author.avatar.url if message.author.avatar else message.author.default_avatar.url
+                )
+                embed.set_image(url=url)
+
+                await message.channel.send(embed=embed)
+            except discord.Forbidden:
+                print("🚫 Permissions manquantes.")
+        else:
+            try:
+                await message.delete()
+
+                embed = discord.Embed(
+                    description="**Lien interdit supprimé.**",
+                    color=discord.Color.red()
+                )
+                embed.set_author(
+                    name=str(message.author),
+                    icon_url=message.author.avatar.url if message.author.avatar else message.author.default_avatar.url
+                )
                 await message.channel.send(embed=embed, delete_after=5)
             except discord.Forbidden:
-                print("🚫 Permissions manquantes pour supprimer ou envoyer un embed.")
-            break
+                print("🚫 Permissions manquantes.")
+        break
 
 bot.run(os.getenv("DISCORD_TOKEN"))
+
